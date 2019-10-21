b100e3e86d1b9fa155957dee12223220f39a2ac7
fuchami <famichiki.yuuki@gmail.com>
2019-10-22 04:06:19 +0900

    ResNet実装まで

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # scene_detection_pytorch
 My research theme  
 
+# TODO List
++ [x]: 特徴抽出モデルまで
++ [ ]: データの前処理
++ [ ]: DataLoaderの実装
++ [ ]: SiameseNetの実装
++ [ ]: 学習フェーズの実装
+
 
 # Code Structure
 - **data_preprocessing.py**
---
+++ models/networks.py
@@ -0,0 +1,55 @@
+# coding:utf-8
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+
+from torchsummary import summary
+
+class ImageExtractor(nn.Module):
+    def __init__(self, model='res'):
+        """
+        res: ResNet-152
+        vgg: VGG-16
+        """
+        super(ImageExtractor, self).__init__()
+        if model=='res':
+            """ load ResNet-152 """
+            resnet = models.resnet152(pretrained=True)
+            self.trained_model = nn.Sequential(*list(resnet.children())[:-1])
+        
+        # Freeze layer
+        for param in self.trained_model.parameters():
+            param.requires_grad = False
+    
+    def forward(self, x):
+        x = self.trained_model(x)
+        x = x.view(x.size()[0], -1)
+        return x
+
+class SiameseNet(nn.Module):
+    def __init__(self, image_extractor):
+        super(SiameseNet, self).__init__()
+        self.image_extractor = image_extractor
+
+        self.fc = nn.Sequential(nn.Linear(2048, 512), nn.PReLU(),
+                                nn.Linear(512, 30), nn.PReLU(),
+                                nn.Linear(30, 1))
+    
+    def forward(self, x1, x2):
+        output1 = self.image_extractor(x1)
+        output1 = self.fc(output1)
+        output2 = self.image_extractor(x2)
+        output2 = self.fc(output2)
+
+        return output1, output2
+
+    def get_embedding(self, x):
+        return self.image_extractor(x)
+
+if __name__ == "__main__":
+
+    image_extractor = ImageExtractor()
+    siamesenet = SiameseNet(image_extractor)
+    print(siamesenet)
+    summary(siamesenet, [(3,224,224), (3,224,224)])
